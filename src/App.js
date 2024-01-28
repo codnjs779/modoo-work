95f4b23e9dd0b164887536fbf260218ee19b2de5
chaewon <codnjs779@naver.com>
2024-01-28 17:10:24 +0900

    Feat: 프로젝트 구조 및 theme 파일

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import { ThemeProvider } from 'styled-components';
 import './App.css';
+import Header from './components/Header/Header';
+import SearchBar from './components/SearchBar/SearchBar';
+import { theme } from './default/theme';
+import ItemList from './components/ItemList/ItemList';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+      <Header/>
+      <SearchBar/>
+      <ItemList/>
     </div>
+    </ThemeProvider>
   );
 }
 
---
+++ src/default/theme.js
@@ -0,0 +1,73 @@
+import { BookmarkBorder as BookmarkBorderIcon, Bookmark as BookmarkIcon } from '@mui/icons-material';
+
+
+const windowSize = {
+    small: '600px',
+    base: '768px',
+    large: '1024px'
+};
+
+const fontSize = {
+  xs: '0.65rem',
+  sm: '0.85rem',
+  base: '1rem',
+  md: '1.25rem',
+  lg: '1.5rem',
+};
+
+const colorPalette = {
+    lightGray:"#ccc",
+    darkGray:"#737373",
+    mainColor:"#488aa2",
+    baseColor:"black",
+    grayShdow:"rgba(0, 0, 0, 0.1)"
+}
+
+const radius = {
+    sm: '6px',
+    base:'10px',
+    md:'15px',
+    lg:'25px'
+}
+
+const fontBold = {
+    thin:'300',
+    base:'500',
+    mdBold:'600',
+    lgBold:'700',
+}
+
+// const iconType = {
+// baseBookmark: BookmarkBorderIcon,
+// checkedBookmark:BookmarkIcon,
+// };
+
+// const iconSize = {
+//     lg: {
+//         width: '25px',
+//         height:'25px',
+//     },
+//     md: {
+//         width:'20px',
+//         height:'20px',
+//     },
+//     sm: {
+//         width: '15px',
+//         height: '15px',
+//     },
+// };
+
+// const baseStyle = {
+//     display:'inline-flex',
+//     justifyContent:'center',
+//     alignItems:'center',
+// };
+
+export const theme = {
+    windowSize,
+    fontSize,
+    colorPalette,
+    radius,
+    fontBold,
+}
+
