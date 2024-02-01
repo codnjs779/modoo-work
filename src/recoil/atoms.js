352e30f77eff8dd239d5700c3b0e14a4c20594b8
chaewon <codnjs779@naver.com>
2024-02-01 18:13:17 +0900

    Feat: recoil state, selector

--- src/recoil/atoms.js
+++ src/recoil/atoms.js
@@ -1,6 +1,16 @@
 import { atom } from "recoil"
+import { axiosData } from "../apis/jobsData"
 
 export const jobsListState = atom({
     key:'jobListState',
-    default: [],
+    default: {
+        processedJobs: [],
+      },
 })
+
+export const infiniteScrollState = atom({
+    key:'infiniteScroll',
+    default:1,
+})
+
+
--- src/recoil/selector.js
+++ src/recoil/selector.js
@@ -1,11 +1,12 @@
 import { selector, selectorFamily } from "recoil";
 import { axiosData } from "../apis/jobsData";
-
+import { jobsListState } from "./atoms";
 
 export const fetchJobData = selectorFamily({
-    key:'fetchJobData',
-    get: (params) => 
-    async () => {
-        return await axiosData(params)
-    }
-})
+    key: 'fetchJobData',
+    get: (params) => async () => {
+        // Fetch data based on the provided nm and row parameters
+        const res = await axiosData({ nm:params.nm, row:params.row });
+        return res;
+    },
+});
